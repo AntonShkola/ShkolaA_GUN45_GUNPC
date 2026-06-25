82e79c9a3ed68ac9562890e2db9c3d4b0b254c22
AntonShkola <anton.shkola@yandex.ru>
2026-06-25 08:36:09 +0300

    изучение массива

---
+++ arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[][] array  = new int[3][] 
+{ 
+    new[] { 12, 2, 3 }, 
+    new[] { 13, 2, 3 }, 
+    new[] { 32, 2, 3 } 
+};
+
+//Console.WriteLine(array[0]);
+//Console.WriteLine(array[2][0]);
+
+
+int[] testArray = new[] { 3, 2, 1, 4, 12 };
+Array.Sort(testArray, 0, 4);
+Console.WriteLine(testArray[0]);
+Console.WriteLine(testArray[1]);
+Console.WriteLine(testArray[2]);
+Console.WriteLine(testArray[3]);
+Console.WriteLine(testArray[4]);
