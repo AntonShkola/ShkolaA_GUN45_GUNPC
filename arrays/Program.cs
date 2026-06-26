3b8f2ca95bb4becd8b49c4730ada1145e4b5277a
AntonShkola <anton.shkola@yandex.ru>
2026-06-26 08:05:57 +0300

    посмотрел видео урок

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -1,19 +1,11 @@
 ﻿
-int[][] array  = new int[3][] 
-{ 
-    new[] { 12, 2, 3 }, 
-    new[] { 13, 2, 3 }, 
-    new[] { 32, 2, 3 } 
-};
+int[] array1 = new[] { 42, 3, 12, 552 };
+float[] array2 = new[] { 12.56f, 6.5f, 72.1f, 32, 98f };
 
-//Console.WriteLine(array[0]);
-//Console.WriteLine(array[2][0]);
+var results = Array.CreateInstance(typeof(float), 4);
+results.SetValue(array1[0] * array2[0], 0);
+//Console.WriteLine(results.GetValue(0));
 
-
-int[] testArray = new[] { 3, 2, 1, 4, 12 };
-Array.Sort(testArray, 0, 4);
-Console.WriteLine(testArray[0]);
-Console.WriteLine(testArray[1]);
-Console.WriteLine(testArray[2]);
-Console.WriteLine(testArray[3]);
-Console.WriteLine(testArray[4]);
+//Array.Reverse(array2);
+Console.WriteLine(array2.Min());
+Console.WriteLine(array2.Max());
