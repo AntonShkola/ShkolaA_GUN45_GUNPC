6ab9105241753202b2c8250f8bd35d4ae24bcd81
AntonShkola <anton.shkola@yandex.ru>
2026-07-07 21:06:13 +0300

    homework correct

--- cycles/Program.cs
+++ cycles/Program.cs
@@ -1,28 +1,36 @@
 ﻿
-class Programm
+class Program
 {
     static void Main()
     {
         // 1
 
-        int[] arrayFib = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 33 };
+        int[] arrayFib = new int[10]; 
+        arrayFib[0] = 0;
+        arrayFib[1] = 1;
 
-        foreach (int i in arrayFib)
-        {
-            Console.WriteLine(i);
+        Console.WriteLine(arrayFib[0]);
+        Console.WriteLine(arrayFib[1]);
+
+        for (int i = 2; i < arrayFib.Length; i++)
+        {                 
+            arrayFib[i] = arrayFib[i-1] + arrayFib[i-2];
+            Console.WriteLine(arrayFib[i]);
         }
 
         Console.WriteLine();
 
         // 2
 
-        for (int i = 0; i <= 20; i += 2)
+        for (int i = 2; i <= 20; i += 2)
         {
             Console.WriteLine(i);
         }
 
+        Console.WriteLine();
+
         // 3
-     
+
         for (int i = 1; i <= 5; i++)
         {       
             for (int j = 1; j <= 5; j++)
@@ -32,6 +40,8 @@ class Programm
             }
             Console.WriteLine();
         }
+
+        Console.WriteLine();
 
         // 4
 
@@ -43,7 +53,7 @@ class Programm
             Console.WriteLine("Please, enter the password");
             userValue = Console.ReadLine();
 
-        } while (!(userValue == password));
+        } while (userValue != password);
 
         Console.WriteLine("Succes!");
     }
