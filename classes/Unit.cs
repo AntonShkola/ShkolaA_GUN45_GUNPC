e9fc50ba8de0bd4cb777969e5cfdae99cf28af47
AntonShkola <anton.shkola@yandex.ru>
2026-07-19 12:19:49 +0300

    2 конструктора

---
+++ classes/Unit.cs
@@ -0,0 +1,18 @@
+﻿
+
+public class Unit
+{
+    private float _health; // поле
+
+    public string Name { get; } // свойство
+
+    public float Health => _health; // свойство
+
+    public Unit(): this(name:"Unknown name") // конструктор без аргуементо вызываюещего
+    {
+    }
+    public Unit(string name) // конструктор с прокидыванием значения
+    {
+        Name = name;
+    }
+}
