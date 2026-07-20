015de7aa2866b2e686566592b830793bc6a91ff6
AntonShkola <a.shkola@carx-tech.com>
2026-07-20 15:50:42 +0300

    конструкторы снова

--- classes/Unit.cs
+++ classes/Unit.cs
@@ -5,10 +5,9 @@ public class Unit
     private float _health; // поле
 
     public string Name { get; } // свойство
-
     public float Health => _health; // свойство
 
-    public Unit(): this(name:"Unknown name") // конструктор без аргуементо вызываюещего
+    public Unit(): this(name:"Default name") // конструктор без аргуементо вызываюещего
     {
     }
     public Unit(string name) // конструктор с прокидыванием значения
