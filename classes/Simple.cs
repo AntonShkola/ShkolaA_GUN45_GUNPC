504e56e391ef6dc430d98adff132beb9affbe22e
AntonShkola <a.shkola@carx-tech.com>
2026-08-20 08:57:09 +0300

    simple-classes урок №55

--- classes/Simple.cs
+++ classes/Simple.cs
@@ -6,11 +6,30 @@ namespace Lesson
 {
     class Program
     {
+        enum Color
+        {
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Orange
+        }
 
         class Point
         {
             public int x;
             public int y;
+            public Color color;
+        }
+
+        class Stident
+        {
+            public Guid id;
+            public string firstName;
+            public string lastName;
+            public string middleName;
+            public int age;
+            public string group;
         }
 
         static void Main(string[] args)
@@ -18,19 +37,9 @@ namespace Lesson
             Point p = new Point();
             p.x = 4;
             p.y = 2;
+            p.color = Color.Red;
 
-            p = null;
-
-            Point p2 = new Point();
-            p2.x = 2;
-            p2.y = 6;
-
-            Console.WriteLine(p.x);
-            Console.WriteLine(p.y);
-
-            Console.WriteLine(p2.x);
-            Console.WriteLine(p2.y);
-          
-;        }
+            Console.WriteLine($"X: {p.x} | Y: {p.y} | Color: {p.color}");          
+        }
     }
 }
